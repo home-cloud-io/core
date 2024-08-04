13152fd5fd9f1cab7872461d35eeec66317ed32e
Jack Kawell <jack@kawell.us>
2024-08-03 20:35:40 -0500

    rpc web install, update, and delete

--- services/platform/server/web/rpc.go
+++ services/platform/server/web/rpc.go
@@ -5,7 +5,9 @@ import (
 
 	v1 "github.com/home-cloud-io/core/api/platform/server/v1"
 	sdConnect "github.com/home-cloud-io/core/api/platform/server/v1/v1connect"
+	opv1 "github.com/home-cloud-io/core/services/platform/operator/api/v1"
 	"github.com/home-cloud-io/core/services/platform/server/daemon"
+	k8sclient "github.com/home-cloud-io/core/services/platform/server/k8s-client"
 
 	"connectrpc.com/connect"
 	"github.com/steady-bytes/draft/pkg/chassis"
@@ -18,13 +20,15 @@ type (
 	}
 
 	rpc struct {
-		logger chassis.Logger
+		logger    chassis.Logger
+		k8sclient k8sclient.Client
 	}
 )
 
 func New(logger chassis.Logger) Rpc {
 	return &rpc{
 		logger: logger,
+		k8sclient: k8sclient.NewClient(logger),
 	}
 }
 
@@ -50,4 +54,49 @@ func (h *rpc) RestartHost(ctx context.Context, request *connect.Request[v1.Resta
 		return nil, err
 	}
 	return connect.NewResponse(&v1.RestartHostResponse{}), nil
+}
+
+func (h *rpc) InstallApp(ctx context.Context, request *connect.Request[v1.InstallAppRequest]) (*connect.Response[v1.InstallAppResponse], error) {
+	h.logger.WithField("request", request.Msg).Info("install request")
+	err := h.k8sclient.Install(ctx, opv1.AppSpec{
+		Chart: request.Msg.Chart,
+		Repo: request.Msg.Repo,
+		Release: request.Msg.Release,
+		Values: request.Msg.Values,
+	})
+	if err != nil {
+		h.logger.WithError(err).Error("failed to install app")
+		return nil, err
+	}
+	h.logger.Info("finished request")
+	return connect.NewResponse(&v1.InstallAppResponse{}), nil
+}
+
+func (h *rpc) DeleteApp(ctx context.Context, request *connect.Request[v1.DeleteAppRequest]) (*connect.Response[v1.DeleteAppResponse], error) {
+	h.logger.WithField("request", request.Msg).Info("delete request")
+	err := h.k8sclient.Delete(ctx, opv1.AppSpec{
+		Release: request.Msg.Release,
+	})
+	if err != nil {
+		h.logger.WithError(err).Error("failed to delete app")
+		return nil, err
+	}
+	h.logger.Info("finished request")
+	return connect.NewResponse(&v1.DeleteAppResponse{}), nil
+}
+
+func (h *rpc) UpdateApp(ctx context.Context, request *connect.Request[v1.UpdateAppRequest]) (*connect.Response[v1.UpdateAppResponse], error) {
+	h.logger.WithField("request", request.Msg).Info("update request")
+	err := h.k8sclient.Update(ctx, opv1.AppSpec{
+		Chart: request.Msg.Chart,
+		Repo: request.Msg.Repo,
+		Release: request.Msg.Release,
+		Values: request.Msg.Values,
+	})
+	if err != nil {
+		h.logger.WithError(err).Error("failed to update app")
+		return nil, err
+	}
+	h.logger.Info("finished request")
+	return connect.NewResponse(&v1.UpdateAppResponse{}), nil
 }
