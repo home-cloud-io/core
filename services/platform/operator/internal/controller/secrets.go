c80dff972abe38e611b4d95b52994e1e2426c432
Jack Kawell <jack@kawell.us>
2024-08-07 00:17:00 -0500

    secret creator

---
+++ services/platform/operator/internal/controller/secrets.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/home-cloud-io/core/services/platform/operator/internal/controller/secrets"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r *AppReconciler) createSecret(ctx context.Context, s AppSecret, namespace string) error {
+	// generate secret for each key
+	data := map[string][]byte{}
+	for _, k := range s.Keys {
+		if k.Length == 0 {
+			k.Length = 24
+		}
+		data[k.Name] = secrets.Generate(k.Length, k.NoSpecialCharacters)
+	}
+
+	// create secret on cluster
+	err := r.Client.Create(ctx, &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      s.Name,
+			Namespace: namespace,
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: data,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
