fae59f108764410f4d50c15f010633b7ced99f26
Jack Kawell <jack@kawell.us>
2024-08-07 00:16:34 -0500

    secret generator

---
+++ services/platform/operator/internal/controller/secrets/generator.go
@@ -0,0 +1,48 @@
+package secrets
+
+import (
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+func Generate(passwordLength int, disableSpecialCharacters bool) []byte {
+	// Character sets for generating passwords
+	lowerCase := "abcdefghijklmnopqrstuvwxyz" // lowercase
+	upperCase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" // uppercase
+	numbers := "0123456789"                   // numbers
+	specialChar := "!@#$%^&*()_-+={}[/?]"     // special characters
+
+	// Variable for storing password
+	password := ""
+
+	// Initialize the random number generator
+	source := rand.NewSource(uint64(time.Now().UnixNano()))
+	rng := rand.New(source)
+
+	// Generate password character by character
+	for n := 0; n < passwordLength; n++ {
+		// Generate a random number to choose a character set
+		randNum := rng.Intn(4)
+		if disableSpecialCharacters {
+			randNum = rng.Intn(3)
+		}
+
+		switch randNum {
+		case 0:
+			randCharNum := rng.Intn(len(lowerCase))
+			password += string(lowerCase[randCharNum])
+		case 1:
+			randCharNum := rng.Intn(len(upperCase))
+			password += string(upperCase[randCharNum])
+		case 2:
+			randCharNum := rng.Intn(len(numbers))
+			password += string(numbers[randCharNum])
+		case 3:
+			randCharNum := rng.Intn(len(specialChar))
+			password += string(specialChar[randCharNum])
+		}
+	}
+
+	return []byte(password)
+}
