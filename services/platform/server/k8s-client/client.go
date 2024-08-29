022dc52538ccdfbd6fbad07f064666aaae08fe40
Jack Kawell <jack@kawell.us>
2024-08-29 15:12:28 -0500

    feat: app healthchecks (#31)
    Implement the app healthchecks API.

--- services/platform/server/k8s-client/client.go
+++ services/platform/server/k8s-client/client.go
@@ -22,6 +22,7 @@ type (
 		Update(ctx context.Context, spec opv1.AppSpec) error
 
 		CurrentContainerVersions(ctx context.Context) ([]*webv1.ImageVersion, error)
+		CheckAppsHealth(ctx context.Context) ([]*webv1.AppHealth, error)
 	}
 
 	client struct {
@@ -117,6 +118,46 @@ func (c *client) CurrentContainerVersions(ctx context.Context) ([]*webv1.ImageVe
 	}
 
 	return imagesSlice, nil
+}
+
+func (c *client) CheckAppsHealth(ctx context.Context) ([]*webv1.AppHealth, error) {
+
+	// get all installed apps
+	apps := &opv1.AppList{}
+	err := c.client.List(ctx, apps, &crclient.ListOptions{
+		Namespace: homeCloudNamespace,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// process each app and check all app pods for status
+	checks := make([]*webv1.AppHealth, len(apps.Items))
+	for index, app := range apps.Items {
+		checks[index] = &webv1.AppHealth{
+			Name:   app.Name,
+			Status: webv1.AppStatus_APP_STATUS_HEALTHY,
+		}
+
+		// get all pods in app namespace
+		pods := &corev1.PodList{}
+		err := c.client.List(ctx, pods, &crclient.ListOptions{
+			Namespace: app.Name,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, pod := range pods.Items {
+			// if any pod isn't in running status mark app as unhealthy and break
+			if pod.Status.Phase != corev1.PodRunning {
+				checks[index].Status = webv1.AppStatus_APP_STATUS_UNHEALTHY
+				break
+			}
+		}
+	}
+
+	return checks, nil
 }
 
 func (c *client) getCurrentImageVersions(ctx context.Context, namespace string, images map[string]*webv1.ImageVersion) error {
