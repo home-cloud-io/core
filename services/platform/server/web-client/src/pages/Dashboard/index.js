e52897e8d21a123259522b927380acae136d8799
Andrew Meiling <AndrewSC208@users.noreply.github.com>
2024-09-02 15:13:45 -0500

    fix if no apps installed render still render home screen, and nav on home screen (#62)
    fix if no apps installed render still render home screen, and nav on
    home screen

--- services/platform/server/web-client/src/pages/Dashboard/index.js
+++ services/platform/server/web-client/src/pages/Dashboard/index.js
@@ -50,7 +50,7 @@ export default function DashboardPage() {
     <>
       <Routes>
         <Route path="/" element={<DefaultLayout />} >
-          <Route index path="home" element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
           <Route path="store" element={<AppStorePage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
