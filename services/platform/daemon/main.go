af14212de4dd4c53ef9847472bb4cfd97964a6e1
Jack Kawell <jack@kawell.us>
2024-09-02 22:22:22 -0500

    chore: remove set IP at boot (daemon) (#64)
    This code is no longer needed now that we're using a NodeIP for the
    Envoy k8s service.

--- services/platform/daemon/main.go
+++ services/platform/daemon/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/home-cloud-io/core/services/platform/daemon/communicate"
-	"github.com/home-cloud-io/core/services/platform/daemon/host"
 	"github.com/steady-bytes/draft/pkg/chassis"
 	"github.com/steady-bytes/draft/pkg/loggers/zerolog"
 )
@@ -11,11 +10,9 @@ func main() {
 	var (
 		logger = zerolog.New()
 		client = communicate.NewClient(logger)
-		runner = host.NewRunner(logger)
 	)
 
 	defer chassis.New(logger).
 		WithRunner(client.Listen).
-		WithRunner(runner.OnStart).
 		Start()
 }
