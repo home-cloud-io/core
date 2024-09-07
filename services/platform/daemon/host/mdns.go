bc0677e18c028f806a1e281b9e43a80d5c0baa53
Jack Kawell <jack@kawell.us>
2024-09-07 14:22:40 -0500

    fixing config keys

--- services/platform/daemon/host/mdns.go
+++ services/platform/daemon/host/mdns.go
@@ -2,6 +2,7 @@ package host
 
 import (
 	"context"
+	"fmt"
 	"os/exec"
 
 	"github.com/home-cloud-io/core/services/platform/daemon/execute"
@@ -26,20 +27,27 @@ func NewDNSPublisher(logger chassis.Logger) DNSPublisher {
 }
 
 func (p *dnsPublisher) Start() {
+	p.logger.Info("starting mDNS publishing")
 	ctx := context.Background()
 	config := chassis.GetConfig()
 	domain := config.GetString("daemon.domain")
 	if domain == "" {
 		p.logger.Panic("domain not set")
 	}
-	address := config.GetString("domain.address")
-	hostnames := config.GetStringSlice("daemon.hosts")
+	address := config.GetString("daemon.address")
+	hostnames := config.GetStringSlice("daemon.hostnames")
 	for _, hostname := range hostnames {
-		go func(){
-			cmd := exec.Command("avahi-publish", "-a", "-R", hostname, address)
+		go func() {
+			fqdn := fmt.Sprintf("%s.%s", hostname, domain)
+			logger := p.logger.WithFields(chassis.Fields{
+				"hostname": fqdn,
+				"address":  address,
+			})
+			logger.Info("publishing mDNS")
+			cmd := exec.Command("avahi-publish", "-a", "-R", fqdn, address)
 			err := execute.ExecuteCommand(ctx, cmd)
 			if err != nil {
-				p.logger.WithField("hostname", hostname).Panic("failed to publish mDNS")
+				p.logger.Panic("failed to publish mDNS")
 			}
 		}()
 	}
