97966879862cfa54180b4e923146e6ed3f62b258
Jack Kawell <jack@kawell.us>
2024-10-16 11:33:46 -0500

    fixed port

--- services/platform/server/web-client/src/utils/config.js
+++ services/platform/server/web-client/src/utils/config.js
@@ -2,7 +2,7 @@ export let BASE_URL = '';
 export let LOCAL_DOMAIN = 'localhost';
 
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = `http://${LOCAL_DOMAIN}:8002`;
+  BASE_URL = `http://${LOCAL_DOMAIN}:8000`;
 } else {
   BASE_URL = 'http://home-cloud.local';
 }
