bf72688bba213499adb373f233f276be5d7c748d
Jack Kawell <jack@kawell.us>
2024-11-25 17:30:20 -0600

    feat: added `file_path` to `FileDone` message

--- api/platform/daemon/v1/stream_pb.ts
+++ api/platform/daemon/v1/stream_pb.ts
@@ -1352,6 +1352,11 @@ export class FileDone extends Message<FileDone> {
    */
   chunkCount = 0;
 
+  /**
+   * @generated from field: string file_path = 3;
+   */
+  filePath = "";
+
   constructor(data?: PartialMessage<FileDone>) {
     super();
     proto3.util.initPartial(data, this);
@@ -1362,6 +1367,7 @@ export class FileDone extends Message<FileDone> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "file_id", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "chunk_count", kind: "scalar", T: 13 /* ScalarType.UINT32 */ },
+    { no: 3, name: "file_path", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): FileDone {
