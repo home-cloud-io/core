7ba4c9e80b81c1dffab0d6b612261fde67d35e9a
Jack Kawell <jack@kawell.us>
2025-01-23 16:18:58 -0600

    fixing nixos version command

--- services/platform/daemon/host/nix.go
+++ services/platform/daemon/host/nix.go
@@ -105,7 +105,7 @@ func GetNixOSVersion(ctx context.Context, logger chassis.Logger) (string, error)
 	}
 
 	logger.Info("getting NixOS version")
-	cmd = exec.Command("nix-instantiate", "--eval", "--expr", "\"builtins.substring 0 5 ((import <nixos> {}).lib.version)\"")
+	cmd = exec.Command("nix-instantiate", "--eval", "--expr", "'builtins.substring 0 5 ((import <nixos> {}).lib.version)'")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Setpgid: true,
 	}
