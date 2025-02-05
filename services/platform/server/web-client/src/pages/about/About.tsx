eb91796e4b444d797e54605c8c8e22a875d48ddf
Jack Kawell <jack@kawell.us>
2025-02-04 21:45:55 -0600

    fully working client

--- services/platform/server/web-client/src/pages/about/About.tsx
+++ services/platform/server/web-client/src/pages/about/About.tsx
@@ -10,36 +10,27 @@ import {
   List,
   Divider,
   Typography,
-  Skeleton,
-  Select,
-  SelectProps,
+  Table,
+  TableColumnsType,
+  Button,
 } from 'antd';
 import {
-  CheckCircleOutlined,
-  CloseCircleOutlined,
   LoadingOutlined,
+  SyncOutlined,
 } from '@ant-design/icons';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { useQuery } from '@connectrpc/connect-query';
-import { getComponentVersions } from 'api/platform/server/v1/web-WebService_connectquery';
-import logo from '../../assets/logo.png';
 import {
-  LogListener,
-  LogProvider,
-  ProviderValue,
-  useLogs,
-} from '../../services/Logs';
-import { Log } from 'api/platform/server/v1/web_pb';
+  getComponentVersions,
+  getSystemLogs,
+} from 'api/platform/server/v1/web-WebService_connectquery';
+import logo from '../../assets/logo.png';
 
 export default function AboutPage() {
   return (
     <Flex justify="center">
       <Space direction="vertical" size="large" style={{ flex: 'auto' }}>
         <Details />
-        <LogProvider>
-          <LogListener />
-          <Logs />
-        </LogProvider>
+        <Logs />
       </Space>
     </Flex>
   );
@@ -92,117 +83,165 @@ export function Details() {
   );
 }
 
-type DropdownOptions = Map<string, boolean>;
+interface LogType {
+  key: React.Key;
+  source: string;
+  namespace: string;
+  domain: string;
+  log: string;
+  timestamp: Date | undefined;
+}
 
 export function Logs() {
-  const { log, connected } = useLogs() as ProviderValue;
-  const [logs, setLogs] = useState<Log[]>([]);
-  const [limit, setLimit] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [domains, setDomains] = useState<SelectProps['options']>([
-  ]);
-  const [sources, setSources] = useState<SelectProps['options']>([
-  ]);
+  const { data, error, isLoading, isFetching, refetch } = useQuery(
+    getSystemLogs,
+    {
+      sinceSeconds: 300,
+    }
+  );
+  const [logs, setLogs] = useState<LogType[]>([]);
+  const [columns, setColumns] = useState<TableColumnsType<LogType>>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (log) {
-      logs.unshift(log);
-      setLogs(logs);
-      setLimit(limit + 1);
-      if (domains) {
-        let opt = {
-          label: log.domain,
-          value: log.domain,
-        };
-        if (domains.indexOf(opt) === -1) {
-          domains.push(opt);
-          domains.sort();
-          setDomains(domains);
-        }
-      }
-      if (sources) {
-        let opt = {
-          label: log.source,
-          value: log.source,
-        };
-        if (sources.indexOf(opt) === -1) {
-          sources.push(opt);
-          sources.sort();
-          setSources(sources);
-        }
-      }
+    if (data?.logs) {
+      let a: LogType[] = data.logs.map((log, index) => ({
+        key: `${index}`,
+        source: log.source,
+        namespace: log.namespace,
+        domain: log.domain,
+        log: log.log,
+        timestamp: log.timestamp?.toDate(),
+      }));
+      setLogs(a);
     }
-  }, [log]);
 
-  const loadMoreData = () => {
-    if (loading) {
-      return;
+    if (data) {
+      const c: TableColumnsType<LogType> = [
+        {
+          title: 'Timestamp',
+          dataIndex: 'timestamp',
+          sortOrder: 'descend',
+          sorter: (a, b) => {
+            if (a.timestamp && b.timestamp) {
+              return a.timestamp.valueOf() - b.timestamp.valueOf();
+            }
+            return 0;
+          },
+          onFilter: (value, log) => log.domain.includes(value as string),
+          render: (value, record, index) => (
+            <>{record.timestamp?.toLocaleString()}</>
+          ),
+          width: '10%',
+        },
+        {
+          title: 'Domain',
+          dataIndex: 'domain',
+          filters: data.domains.map((x) => ({
+            text: x,
+            value: x,
+          })),
+          filterMode: 'tree',
+          filterSearch: true,
+          onFilter: (value, log) => log.domain.includes(value as string),
+          width: '10%',
+          render: (value, record, index) => (
+            <Tag color={stringToColour(record.domain)}>{record.domain}</Tag>
+          )
+        },
+        {
+          title: 'Group',
+          dataIndex: 'namespace',
+          filters: data.namespaces.map((x) => ({
+            text: x,
+            value: x,
+          })),
+          filterMode: 'tree',
+          filterSearch: true,
+          onFilter: (value, log) => log.namespace.includes(value as string),
+          width: '10%',
+          render: (value, record, index) => (
+            <Tag color={stringToColour(record.namespace)}>{record.namespace}</Tag>
+          )
+        },
+        {
+          title: 'Source',
+          dataIndex: 'source',
+          filters: data.sources.map((x) => ({
+            text: x,
+            value: x,
+          })),
+          filterMode: 'tree',
+          filterSearch: true,
+          onFilter: (value, log) => log.source.includes(value as string),
+          width: '10%',
+          render: (value, record, index) => (
+            <Tag color={stringToColour(record.source)}>{record.source}</Tag>
+          )
+        },
+        {
+          title: 'Log',
+          dataIndex: 'log',
+          width: '70%',
+          render: (value, record, index) => (
+            <Typography
+              style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace' }}
+            >
+              {record.log}
+            </Typography>
+          ),
+        },
+      ];
+      setColumns(c);
     }
-    setLoading(true);
-    let newLimit = limit + 10;
-    if (newLimit > logs.length) {
-      newLimit = logs.length;
-    }
-    setLimit(newLimit);
-  };
+  }, [data]);
 
   return (
     <Card bordered={false}>
       <Flex gap="small" justify="space-between">
         <strong>System Logs</strong>
-        {connected && (
-          <Tag icon={<CheckCircleOutlined />} color="success">
-            Live
-          </Tag>
-        )}
-        {!connected && (
-          <Tag icon={<CloseCircleOutlined />} color="error">
-            Historical
-          </Tag>
-        )}
+        <Button
+          onClick={() => {
+            refetch();
+          }}
+        >
+          <SyncOutlined spin={isFetching} />
+        </Button>
       </Flex>
       <Divider />
-      <Flex gap="small" align="start" justify='start'>
-        <Select
-          mode="multiple"
-          allowClear
-          placeholder="Domains"
-          style={{ width: 120 }}
-          options={domains}
+      {error && (
+        <Alert
+          message="Failed to load device information"
+          description={error.message}
+          type="error"
+          showIcon
         />
-        <Select
-          mode="multiple"
-          allowClear
-          placeholder="Components"
-          style={{ width: 120 }}
-          options={sources}
-        />
-      </Flex>
-      <InfiniteScroll
-        dataLength={limit}
-        next={loadMoreData}
-        hasMore={limit < logs.length}
-        loader={<Skeleton paragraph={{ rows: 1 }} active />}
-        endMessage={<Divider plain>end of logs</Divider>}
-        scrollableTarget="scrollableDiv"
-      >
-        <List
-          size="small"
-          dataSource={logs}
-          renderItem={(log) => (
-            <List.Item>
-              <Flex gap="small" justify="space-between">
-                <strong>{log.source}</strong>
-                <Typography
-                  style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace' }}
-                >
-                  {log.log}
-                </Typography>
-              </Flex>
-            </List.Item>
-          )}
-        />
-      </InfiniteScroll>
+      )}
+      {!error && (
+        <>
+          <Table<LogType>
+            size="small"
+            loading={isLoading}
+            columns={columns}
+            dataSource={logs}
+          />
+        </>
+      )}
     </Card>
   );
 }
+
+/* eslint-disable no-bitwise */
+const stringToColour = (str: string) => {
+  let hash = 0;
+  str.split('').forEach(char => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash)
+  })
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff
+    colour += value.toString(16).padStart(2, '0')
+  }
+  return colour
+}
+/* eslint-enable no-bitwise */
