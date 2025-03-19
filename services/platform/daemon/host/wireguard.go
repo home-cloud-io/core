e82f1da2d2f6fe1d20b108e0db8b9f2b06b77a92
Jack Kawell <jack@kawell.us>
2025-03-19 16:43:05 -0500

    added logging of wireguard commands in daemon

--- services/platform/daemon/host/wireguard.go
+++ services/platform/daemon/host/wireguard.go
@@ -12,6 +12,7 @@ import (
 )
 
 func AddWireguardInterface(ctx context.Context, logger chassis.Logger, def *v1.AddWireguardInterface) error {
+	logger.Info("adding wireguard interface")
 
 	// read config
 	config := NetworkingConfig{}
@@ -102,6 +103,7 @@ func AddWireguardInterface(ctx context.Context, logger chassis.Logger, def *v1.A
 }
 
 func RemoveWireguardInterface(ctx context.Context, logger chassis.Logger, def *v1.RemoveWireguardInterface) error {
+	logger.Info("removing wireguard interface")
 
 	// read config
 	config := NetworkingConfig{}
@@ -177,6 +179,8 @@ func fullWireguardKeyPath(interfaceName string) string {
 }
 
 func AddWireguardPeer(ctx context.Context, logger chassis.Logger, peer *v1.WireguardPeer) error {
+	logger.Info("adding wireguard peer")
+
 	// read config
 	config := NetworkingConfig{}
 	f, err := os.ReadFile(NetworkingConfigFile())
