fcd6fab12fdf0da0535ea2a460ece1e221627a0b
Jack Kawell <jack@kawell.us>
2025-04-15 16:25:31 -0500

    working on new settings card

--- services/platform/server/web-client/src/pages/home/Home.tsx
+++ services/platform/server/web-client/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   Flex,
   Space,
@@ -10,25 +10,20 @@ import {
   Badge,
   Alert,
   Avatar,
-  Empty,
-  Button,
-  Tooltip,
-  ProgressProps,
-} from "antd";
+} from 'antd';
 import {
   CheckCircleOutlined,
   CloseCircleOutlined,
   LoadingOutlined,
-} from "@ant-design/icons";
-import { AppHealth, AppStatus } from "api/platform/server/v1/web_pb";
-import { useQuery } from "@connectrpc/connect-query";
+} from '@ant-design/icons';
+import { AppHealth, AppStatus } from 'api/platform/server/v1/web_pb';
+import { useQuery } from '@connectrpc/connect-query';
 import {
   appsHealthCheck,
   getSystemStats,
-} from "api/platform/server/v1/web-WebService_connectquery";
-import { SystemStats } from "api/platform/daemon/v1/system_pb";
-import { ProviderValue, useEvents } from "../../services/Subscribe";
-import { useNavigate } from "react-router-dom";
+} from 'api/platform/server/v1/web-WebService_connectquery';
+import { SystemStats } from 'api/platform/daemon/v1/system_pb';
+import { ProviderValue, useEvents } from '../../services/Subscribe';
 
 export default function HomePage() {
   return (
@@ -36,7 +31,7 @@ export default function HomePage() {
       <Space
         direction="vertical"
         size="large"
-        style={{ maxWidth: 450, flex: "auto" }}
+        style={{ maxWidth: 450, flex: 'auto' }}
       >
         <DeviceDetails />
         <InstalledApplicationsList />
@@ -46,15 +41,29 @@ export default function HomePage() {
 }
 
 export function DeviceDetails() {
-  const { data, error, isLoading } = useQuery(getSystemStats, undefined, {
-    refetchInterval: 2000,
-  });
+  const { data, error, isLoading } = useQuery(getSystemStats);
   const { connected } = useEvents() as ProviderValue;
 
   var stats = new SystemStats();
   if (data?.stats) {
     stats = data.stats;
   }
+
+  const formatBytes = (bytes: number, decimals = 2) => {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  };
+
+  const formatPercentage = (free: number, total: number) => {
+    return Math.round(((total - free) / total) * 100);
+  };
 
   return (
     <Card bordered={false}>
@@ -72,7 +81,7 @@ export function DeviceDetails() {
         )}
       </Flex>
       <Divider />
-      <Space direction="vertical" size="small" style={{ display: "flex" }}>
+      <Space direction="vertical" size="small" style={{ display: 'flex' }}>
         <strong>Storage</strong>
         {isLoading && (
           <Spin indicator={<LoadingOutlined spin />} size="large" />
@@ -85,99 +94,29 @@ export function DeviceDetails() {
             showIcon
           />
         )}
-        {!isLoading && !error && <DriveList stats={stats} />}
-        <strong>System</strong>
-        {isLoading && (
-          <Spin indicator={<LoadingOutlined spin />} size="large" />
+        {!isLoading && !error && (
+          <div>
+            {`${formatBytes(
+              Number(stats.drives[0].totalBytes - stats.drives[0].freeBytes)
+            )} used out of ${formatBytes(
+              Number(stats.drives[0].totalBytes)
+            )} total`}
+            <Progress
+              percent={formatPercentage(
+                Number(stats.drives[0].freeBytes),
+                Number(stats.drives[0].totalBytes)
+              )}
+              percentPosition={{ align: 'start', type: 'outer' }}
+            />
+          </div>
         )}
-        {error && (
-          <Alert
-            message="Failed to load system stats"
-            description={error.message}
-            type="error"
-            showIcon
-          />
-        )}
-        {!isLoading && !error && <System stats={stats} />}
       </Space>
     </Card>
   );
 }
 
-type SystemStatsProps = {
-  stats: SystemStats;
-};
-
-export function DriveList(props: SystemStatsProps) {
-  return (
-    <Flex gap="large" justify="space-around" wrap>
-      {props.stats.drives.map((drive) => {
-        return (
-          <Tooltip
-            key={drive.mountPoint}
-            title={`Free: ${formatBytes(Number(drive.freeBytes))}`}
-            placement={"bottom"}
-          >
-            <Flex vertical justify="center" gap="small">
-              <Badge count={driveName(drive.mountPoint)} color={"#643f91"}>
-                <Progress
-                  type="dashboard"
-                  strokeColor={progressColors}
-                  status="normal"
-                  percent={formatPercentage(
-                    Number(drive.freeBytes),
-                    Number(drive.totalBytes)
-                  )}
-                  percentPosition={{ align: "start", type: "outer" }}
-                />
-              </Badge>
-            </Flex>
-          </Tooltip>
-        );
-      })}
-    </Flex>
-  );
-}
-
-export function System(props: SystemStatsProps) {
-  let totalFreeMemory =
-    Number(props.stats.memory?.totalBytes) -
-    Number(props.stats.memory?.usedBytes);
-  return (
-    <Flex gap="large" justify="space-around" wrap>
-      <Badge count={"CPU"} color={"#643f91"}>
-        <Progress
-          type="dashboard"
-          strokeColor={progressColors}
-          status="normal"
-          percent={Math.round(Number(props.stats.compute?.userPercent))}
-          percentPosition={{ align: "start", type: "outer" }}
-        />
-      </Badge>
-      <Tooltip
-        title={`Free: ${formatBytes(totalFreeMemory)}`}
-        placement={"bottom"}
-      >
-        <Badge count={"Memory"} color={"#643f91"}>
-          <Progress
-            type="dashboard"
-            strokeColor={progressColors}
-            status="normal"
-            percent={formatPercentage(
-              totalFreeMemory,
-              Number(props.stats.memory?.totalBytes)
-            )}
-            percentPosition={{ align: "start", type: "outer" }}
-          />
-        </Badge>
-      </Tooltip>
-    </Flex>
-  );
-}
-
 export function InstalledApplicationsList() {
   const { data, error, isLoading } = useQuery(appsHealthCheck);
-  const navigate = useNavigate();
 
   var checks: AppHealth[] = [];
   if (data?.checks) {
@@ -196,16 +135,7 @@ export function InstalledApplicationsList() {
         );
       }
     }
-    return (
-      <Empty
-        image={Empty.PRESENTED_IMAGE_SIMPLE}
-        description={"No apps installed"}
-      >
-        <Button type="primary" onClick={() => navigate("/store")}>
-          App Store
-        </Button>
-      </Empty>
-    );
+    return <p>None</p>;
   };
 
   return (
@@ -233,13 +163,13 @@ type Props = {
 function Application(props: Props) {
   const app = props.app;
 
-  var status: "error" | "success" = "success";
+  var status: 'error' | 'success' = 'success';
   if (props.app.status !== AppStatus.HEALTHY) {
-    status = "error";
+    status = 'error';
   }
 
   return (
-    <div style={{ padding: 4, width: 64, textAlign: "center" }}>
+    <div style={{ padding: 4, width: 64, textAlign: 'center' }}>
       <Badge dot status={status}>
         <Avatar src={app.display?.iconUrl} shape="square" size="large" />
       </Badge>
@@ -247,36 +177,3 @@ function Application(props: Props) {
     </div>
   );
 }
-
-// HELPERS
-
-function driveName(mountPoint: string) {
-  switch (mountPoint) {
-    case "/":
-      return "System";
-    default:
-      return "Apps";
-  }
-}
-
-const progressColors: ProgressProps["strokeColor"] = {
-  "0%": "#643f91",
-  "75%": "#ffe58f",
-  "100%": "#ff4d4f",
-};
-
-const formatBytes = (bytes: number, decimals = 2) => {
-  if (bytes === 0) return "0 Bytes";
-
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-};
-
-const formatPercentage = (free: number, total: number) => {
-  return Math.round(((total - free) / total) * 100);
-};
