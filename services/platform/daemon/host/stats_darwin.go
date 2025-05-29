002339198297af499d5a1b5778cb149cf6adf983
Jack Kawell <jack@kawell.us>
2025-05-29 12:38:00 -0500

    add comments

--- services/platform/daemon/host/stats_darwin.go
+++ services/platform/daemon/host/stats_darwin.go
@@ -3,12 +3,18 @@
 
 package host
 
+// NOTE: the daemon is not intended to be run on darwin systems except for development
+// purposes. The below stats are limited on darwin builds only to enable build for local
+// development.
+
 import (
 	v1 "github.com/home-cloud-io/core/api/platform/daemon/v1"
 
 	"github.com/mackerelio/go-osstat/memory"
 )
 
+// NOTE: darwin builds do not support compute stats and so this will only ever
+// return zeros as values.
 func getComputeStats(stats *v1.SystemStats) error {
 	stats.Compute = &v1.ComputeStats{
 		UserPercent:   0,
@@ -18,6 +24,8 @@ func getComputeStats(stats *v1.SystemStats) error {
 	return nil
 }
 
+// NOTE: darwin builds do not support Available memory calls so this value
+// will only ever be zero.
 func getMemoryStats(stats *v1.SystemStats) error {
 	memory, err := memory.Get()
 	if err != nil {
