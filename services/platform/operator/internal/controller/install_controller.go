33947756dcc6a6e1ad0bb485d491fff5eccbf04a
Jack Kawell <jack@kawell.us>
2025-09-27 23:48:07 -0500

    wip: more work on install (gateway api, draft and home cloud server)

--- services/platform/operator/internal/controller/install_controller.go
+++ services/platform/operator/internal/controller/install_controller.go
@@ -2,35 +2,38 @@ package controller
 
 import (
 	"context"
+	"errors"
+	"io"
+	"net/http"
+	"slices"
+	"time"
 
+	"gopkg.in/yaml.v3"
 	"helm.sh/helm/v3/pkg/action"
-	appsv1 "k8s.io/api/apps/v1"
-	apiv1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/ptr"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	v1 "github.com/home-cloud-io/core/services/platform/operator/api/v1"
+	resources "github.com/home-cloud-io/core/services/platform/operator/internal/controller/resources"
 )
 
 // InstallReconciler reconciles a Install object
 type InstallReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Config *rest.Config
 }
 
 const (
 	InstallFinalizer = "install.home-cloud.io/finalizer"
-
-	defaultIstioVersion   = "1.27.1"
-	defaultIstioNamespace = "istio-system"
-	defaultIstioRepoURL   = "https://istio-release.storage.googleapis.com/charts"
 )
 
 //+kubebuilder:rbac:groups=home-cloud.io,resources=installs,verbs=get;list;watch;create;update;patch;delete
@@ -45,7 +48,7 @@ func (r *InstallReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	install := &v1.Install{}
 	err := r.Get(ctx, req.NamespacedName, install)
 	if err != nil {
-		if errors.IsNotFound(err) {
+		if kerrors.IsNotFound(err) {
 			l.Info("Install resource not found. Assuming this means the resource was deleted and so ignoring.")
 			return ctrl.Result{}, nil
 		}
@@ -77,19 +80,28 @@ func (r *InstallReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 func (r *InstallReconciler) install(ctx context.Context, install *v1.Install) error {
 	l := log.FromContext(ctx)
 
-	actionConfiguration, err := createHelmAction(defaultIstioNamespace)
+	// install gateway api
+	resp, err := http.Get("https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml")
+	if err != nil {
+		return err
+	}
+	err = r.apply(ctx, resp.Body)
 	if err != nil {
 		return err
 	}
 
+	actionConfiguration, err := createHelmAction(resources.DefaultIstioNamespace)
+	if err != nil {
+		return err
+	}
 
 	// TODO: genericize this
 	act := action.NewInstall(actionConfiguration)
-	act.Version = defaultIstioVersion
-	act.Namespace = defaultIstioNamespace
-	act.RepoURL = defaultIstioRepoURL
+	act.Version = resources.DefaultIstioVersion
+	act.Namespace = resources.DefaultIstioNamespace
+	act.RepoURL = resources.DefaultIstioRepoURL
 	act.CreateNamespace = true
-
+	act.Wait = true
 
 	// istio base
 	act.ReleaseName = "base"
@@ -135,9 +147,10 @@ func (r *InstallReconciler) install(ctx context.Context, install *v1.Install) er
 	act.ReleaseName = "cni"
 	values = map[string]interface{}{
 		"profile": "ambient",
-		// TODO: need this for k3s install
-		// global:
-		//   platform: k3s
+		// TODO: need this only for k3s install
+		"global": map[string]interface{}{
+			"platform": "k3s",
+		},
 	}
 	exists, err = helmExists(actionConfiguration, act.ReleaseName)
 	if err != nil {
@@ -174,138 +187,26 @@ func (r *InstallReconciler) install(ctx context.Context, install *v1.Install) er
 		}
 	}
 
-	// ingress gateway
 	l.Info("installing ingress gateway")
-	gateway := &gwv1.Gateway{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "ingress-gateway",
-			Namespace: defaultIstioNamespace,
-		},
-		Spec: gwv1.GatewaySpec{
-			GatewayClassName: "istio",
-			Listeners: []gwv1.Listener{
-				{
-					Name: "http",
-					Port: 8080,
-					Protocol: gwv1.HTTPProtocolType,
-					AllowedRoutes: &gwv1.AllowedRoutes{
-						Namespaces: &gwv1.RouteNamespaces{
-							From: ptr.To[gwv1.FromNamespaces]("All"),
-						},
-					},
-				},
-			},
-		},
-	}
-	err = r.Client.Create(ctx, gateway)
+	err = r.Client.Create(ctx, resources.IngressGateway)
 	if client.IgnoreAlreadyExists(err) != nil {
 		return err
 	}
 
-	l.Info("installing default http route")
-	httpRoute := &gwv1.HTTPRoute{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo",
-			Namespace: "default",
-		},
-		Spec: gwv1.HTTPRouteSpec{
-			CommonRouteSpec: gwv1.CommonRouteSpec{
-				ParentRefs: []gwv1.ParentReference{
-					{
-						Name: "ingress-gateway",
-						Namespace: ptr.To[gwv1.Namespace](defaultIstioNamespace),
-					},
-				},
-			},
-			Hostnames: []gwv1.Hostname{
-				"localhost",
-			},
-			Rules: []gwv1.HTTPRouteRule{
-				{
-					BackendRefs: []gwv1.HTTPBackendRef{
-						{
-							BackendRef: gwv1.BackendRef{
-								BackendObjectReference: gwv1.BackendObjectReference{
-									Name: "server",
-									Port: ptr.To[gwv1.PortNumber](80),
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	err = r.Client.Create(ctx, httpRoute)
-	if client.IgnoreAlreadyExists(err) != nil {
-		return err
+	l.Info("installing draft components")
+	for _, o := range resources.DraftObjects {
+		err = r.Client.Create(ctx, o)
+		if client.IgnoreAlreadyExists(err) != nil {
+			return err
+		}
 	}
 
-	// home cloud server
-	l.Info("installing (fake) home cloud server")
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo-deployment",
-			Namespace: "default",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: ptr.To[int32](1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "demo",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "web",
-							Image: "nginx:1.12",
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	err = r.Client.Create(ctx, deployment)
-	if client.IgnoreAlreadyExists(err) != nil {
-		return err
-	}
-
-	l.Info("installing (fake) home cloud server service")
-	service := &apiv1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo",
-			Namespace: "default",
-		},
-		Spec: apiv1.ServiceSpec{
-			Type: apiv1.ServiceTypeClusterIP,
-			Ports: []apiv1.ServicePort{
-				{
-					Name: "http",
-					Port: 80,
-				},
-			},
-			Selector: map[string]string{
-				"app": "demo",
-			},
-		},
-	}
-	err = r.Client.Create(ctx, service)
-	if client.IgnoreAlreadyExists(err) != nil {
-		return err
+	l.Info("installing home cloud server components")
+	for _, o := range resources.HomeCloudServerObjects {
+		err = r.Client.Create(ctx, o)
+		if client.IgnoreAlreadyExists(err) != nil {
+			return err
+		}
 	}
 
 	return r.updateStatus(ctx, install)
@@ -318,32 +219,39 @@ func (r *InstallReconciler) upgrade(ctx context.Context, install *v1.Install) er
 }
 
 func (r *InstallReconciler) uninstall(ctx context.Context, install *v1.Install) error {
-	actionConfiguration, err := createHelmAction(defaultIstioNamespace)
+
+	// NOTE: we do not delete any CRDs (gateway API/istio) as they could be in use by other applications
+
+	for _, o := range slices.Backward(resources.HomeCloudServerObjects) {
+		err := r.Client.Delete(ctx, o)
+		if client.IgnoreNotFound(err) != nil {
+			return err
+		}
+	}
+
+	for _, o := range slices.Backward(resources.DraftObjects) {
+		err := r.Client.Delete(ctx, o)
+		if client.IgnoreNotFound(err) != nil {
+			return err
+		}
+	}
+
+	actionConfiguration, err := createHelmAction(resources.DefaultIstioNamespace)
 	if err != nil {
 		return err
 	}
 
 	act := action.NewUninstall(actionConfiguration)
 	act.IgnoreNotFound = true
+	act.Wait = true
+	act.Timeout = 5 * time.Minute
 
-	_, err = act.Run("istio-base")
-	if err != nil {
-		return err
-	}
-
-	_, err = act.Run("istio-istiod")
-	if err != nil {
-		return err
-	}
-
-	_, err = act.Run("istio-cni")
-	if err != nil {
-		return err
-	}
-
-	_, err = act.Run("istio-ztunnel")
-	if err != nil {
-		return err
+	releases := []string{"ztunnel", "cni", "istiod", "base"}
+	for _, release := range releases {
+		_, err = act.Run(release)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -381,6 +289,52 @@ func (r *InstallReconciler) updateStatus(ctx context.Context, install *v1.Instal
 	install.Status.Version = install.Spec.Version
 	// install.Status.Values = install.Spec.Values
 	return r.Status().Update(ctx, install)
+}
+
+// Apply applies the given YAML manifests to kubernetes
+func (r *InstallReconciler) apply(ctx context.Context, reader io.Reader) error {
+	l := log.FromContext(ctx)
+	dynamicClient, err := dynamic.NewForConfig(r.Config)
+	if err != nil {
+		return err
+	}
+	dec := yaml.NewDecoder(reader)
+	for {
+		// parse the YAML doc
+		obj := &unstructured.Unstructured{Object: map[string]interface{}{}}
+		err := dec.Decode(obj.Object)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if obj.Object == nil {
+			l.Info("skipping empty document")
+			continue
+		}
+		// get GroupVersionResource to invoke the dynamic client
+		gvk := obj.GroupVersionKind()
+		restMapping, err := r.Client.RESTMapper().RESTMapping(gvk.GroupKind(), gvk.Version)
+		if err != nil {
+			return err
+		}
+		gvr := restMapping.Resource
+		// apply the YAML doc
+		namespace := obj.GetNamespace()
+		if len(namespace) == 0 {
+			namespace = "default"
+		}
+
+		applyOpts := metav1.ApplyOptions{FieldManager: "home-cloud-operator"}
+		_, err = dynamicClient.Resource(gvr).Apply(context.TODO(), obj.GetName(), obj, applyOpts)
+		if err != nil {
+			l.Error(err, "failed to apply object", "kind", obj.GetKind(), "name", obj.GetName())
+			return err
+		}
+		l.Info("applied YAML for object", "kind", obj.GetKind(), "name", obj.GetName())
+	}
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
---
+++ services/platform/operator/internal/controller/resources/draft.go
@@ -0,0 +1,155 @@
+package resources
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	DraftObjects = []client.Object{
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "draft-system",
+			},
+		},
+		&corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "blueprint",
+				Namespace: "draft-system",
+			},
+			Spec: corev1.ServiceSpec{
+				Ports: []corev1.ServicePort{
+					{
+						Name: "http",
+						Port: 8090,
+					},
+				},
+				Selector: map[string]string{"app": "blueprint"},
+			},
+		},
+		&corev1.PersistentVolume{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   "blueprint",
+				Labels: map[string]string{"type": "local"},
+			},
+			Spec: corev1.PersistentVolumeSpec{
+				Capacity: corev1.ResourceList{corev1.ResourceName("storage"): resource.MustParse("5G")},
+				// TODO: test this mount
+				PersistentVolumeSource: corev1.PersistentVolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/mnt/k8s-pvs/blueprint"}},
+				AccessModes:            []corev1.PersistentVolumeAccessMode{corev1.PersistentVolumeAccessMode("ReadWriteMany")},
+				ClaimRef: &corev1.ObjectReference{
+					Namespace: "draft-system",
+					Name:      "blueprint",
+				},
+				PersistentVolumeReclaimPolicy: corev1.PersistentVolumeReclaimPolicy("Retain"),
+				StorageClassName:              "manual",
+				NodeAffinity: &corev1.VolumeNodeAffinity{Required: &corev1.NodeSelector{NodeSelectorTerms: []corev1.NodeSelectorTerm{
+					{
+						MatchExpressions: []corev1.NodeSelectorRequirement{
+							{
+								Key:      "kubernetes.io/hostname",
+								Operator: corev1.NodeSelectorOperator("In"),
+								Values:   []string{"home-cloud"},
+							},
+						},
+					},
+				}},
+				},
+			},
+		},
+		&corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "blueprint",
+				Namespace: "draft-system",
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.PersistentVolumeAccessMode("ReadWriteMany")},
+				StorageClassName: ptr.To[string]("manual"),
+			},
+		},
+		&corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "blueprint",
+				Namespace: "draft-system",
+			},
+			Data: map[string]string{"config.yaml": `
+service:
+  name: blueprint
+  domain: core
+  env: prod
+badger:
+  path: /etc/badger/data
+raft:
+  node-id: node_1
+  address: blueprint.draft-system.svc.cluster.local
+  port: 1111
+  bootstrap: true
+`},
+		},
+		&appsv1.StatefulSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "blueprint",
+				Namespace: "draft-system",
+			},
+			Spec: appsv1.StatefulSetSpec{
+				Replicas: ptr.To[int32](1),
+				Selector: &metav1.LabelSelector{MatchLabels: map[string]string{
+					"app": "blueprint",
+				},
+				},
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": "blueprint"}},
+					Spec: corev1.PodSpec{
+						Volumes: []corev1.Volume{
+							{
+								Name: "config",
+								VolumeSource: corev1.VolumeSource{ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{Name: "blueprint"},
+								},
+								},
+							},
+							{
+								Name:         "badger",
+								VolumeSource: corev1.VolumeSource{PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{ClaimName: "blueprint"}},
+							},
+						},
+						Containers: []corev1.Container{
+							{
+								Name:  "blueprint",
+								Image: "ghcr.io/steady-bytes/draft-core-blueprint:v0.0.4",
+								Ports: []corev1.ContainerPort{
+									{
+										Name:          "grpc",
+										ContainerPort: 8090,
+										Protocol:      corev1.Protocol("TCP"),
+									},
+									{
+										Name:          "raft",
+										ContainerPort: 1111,
+										Protocol:      corev1.Protocol("TCP"),
+									},
+								},
+								VolumeMounts: []corev1.VolumeMount{
+									{
+										Name:      "config",
+										MountPath: "/etc/config.yaml",
+										SubPath:   "config.yaml",
+									},
+									{
+										Name:      "badger",
+										MountPath: "/etc/badger/data",
+									},
+								},
+							},
+						},
+					},
+				},
+				ServiceName: "blueprint",
+			},
+		},
+	}
+)
