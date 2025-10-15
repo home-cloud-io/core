c3bcd8b2948ae986658453e214b132a96638cf30
Jack Kawell <jack@kawell.us>
2025-10-14 19:51:41 -0500

    rework server to use new unary daemon api

--- services/platform/server/main.go
+++ services/platform/server/main.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/home-cloud-io/core/services/platform/server/apps"
 	"github.com/home-cloud-io/core/services/platform/server/async"
-	"github.com/home-cloud-io/core/services/platform/server/internal"
 	kvclient "github.com/home-cloud-io/core/services/platform/server/kv-client"
 	"github.com/home-cloud-io/core/services/platform/server/system"
 	"github.com/home-cloud-io/core/services/platform/server/web"
@@ -21,14 +20,10 @@ func main() {
 	var (
 		broadcaster = async.NewBroadcaster()
 		logger      = zerolog.New()
-		daemonRPC   = system.New(logger, broadcaster)
 		actl        = apps.NewController(logger)
 		sctl        = system.NewController(logger, broadcaster)
 		webRPC      = web.New(logger, actl, sctl)
-		webHTTP     = web.NewHttp(logger, actl, sctl)
-		internalRPC = internal.New(logger, sctl)
 	)
-	system.NewCommander(broadcaster)
 
 	runner := func() {
 		kvclient.Init()
@@ -41,10 +36,7 @@ func main() {
 
 	defer chassis.New(logger).
 		WithClientApplication(files).
-		WithRPCHandler(daemonRPC).
 		WithRPCHandler(webRPC).
-		WithRPCHandler(webHTTP).
-		WithRPCHandler(internalRPC).
 		WithRunner(runner).
 		Start()
 }
--- services/platform/server/system/daemon.go
+++ services/platform/server/system/daemon.go
@@ -2,120 +2,35 @@ package system
 
 import (
 	"context"
-	"io"
+
+	"connectrpc.com/connect"
 
 	dv1 "github.com/home-cloud-io/core/api/platform/daemon/v1"
-	"github.com/steady-bytes/draft/pkg/chassis"
 )
 
 type (
 	Daemon interface {
 		// ShutdownHost will shutdown the host machine running Home Cloud.
-		ShutdownHost() error
+		ShutdownHost(ctx context.Context) error
 		// RestartHost will restart the host machine running Home Cloud
-		RestartHost() error
-		// ChangeDaemonVersion will update the NixOS config with a new Daemon version
-		// and switch to it.
-		ChangeDaemonVersion(cmd *dv1.ChangeDaemonVersionCommand) error
-		// AddMdnsHost adds a host to the avahi mDNS server managed by the daemon
-		AddMdnsHost(hostname string) error
-		// RemoveMdnsHost removes a host to the avahi mDNS server managed by the daemon
-		RemoveMdnsHost(hostname string) error
-		// UploadFileStream will stream a file in chunks as an upload to the daemon
-		UploadFileStream(ctx context.Context, logger chassis.Logger, buf io.Reader, fileId, fileName string) (string, error)
+		RestartHost(ctx context.Context) error
 	}
 )
 
 // DAEMON
 
-func (c *controller) ShutdownHost() error {
-	err := com.Send(&dv1.ServerMessage{
-		Message: &dv1.ServerMessage_Shutdown{},
-	})
+func (c *controller) ShutdownHost(ctx context.Context) error {
+	_, err := c.daemonClient.ShutdownHost(ctx, &connect.Request[dv1.ShutdownHostRequest]{})
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c *controller) RestartHost() error {
-	err := com.Send(&dv1.ServerMessage{
-		Message: &dv1.ServerMessage_Restart{},
-	})
+func (c *controller) RestartHost(ctx context.Context) error {
+	_, err := c.daemonClient.RebootHost(ctx, &connect.Request[dv1.RebootHostRequest]{})
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func (c *controller) ChangeDaemonVersion(cmd *dv1.ChangeDaemonVersionCommand) error {
-	err := com.Send(&dv1.ServerMessage{
-		Message: &dv1.ServerMessage_ChangeDaemonVersionCommand{
-			ChangeDaemonVersionCommand: cmd,
-		},
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *controller) AddMdnsHost(hostname string) error {
-	err := com.Send(&dv1.ServerMessage{
-		Message: &dv1.ServerMessage_AddMdnsHostCommand{
-			AddMdnsHostCommand: &dv1.AddMdnsHostCommand{
-				Hostname: hostname,
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *controller) RemoveMdnsHost(hostname string) error {
-	err := com.Send(&dv1.ServerMessage{
-		Message: &dv1.ServerMessage_RemoveMdnsHostCommand{
-			RemoveMdnsHostCommand: &dv1.RemoveMdnsHostCommand{
-				Hostname: hostname,
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *controller) UploadFileStream(ctx context.Context, logger chassis.Logger, buf io.Reader, fileId, fileName string) (string, error) {
-	logger.Info("uploading file")
-
-	_, err := com.Request(ctx, &dv1.ServerMessage{
-		Message: &dv1.ServerMessage_UploadFileRequest{
-			UploadFileRequest: &dv1.UploadFileRequest{
-				Data: &dv1.UploadFileRequest_Info{
-					Info: &dv1.FileInfo{
-						FileId:   fileId,
-						FilePath: fileName,
-					},
-				},
-			},
-		},
-	}, nil)
-	if err != nil {
-		logger.WithError(err).Error("failed to ready daemon for file upload")
-		return fileId, err
-	}
-
-	logger.Info("daemon ready for file upload")
-
-	// chunk file and upload
-	err = c.streamFile(ctx, logger, buf, fileId, fileName)
-	if err != nil {
-		logger.WithError(err).Error("failed to upload chunked file")
-		return fileId, err
-	}
-
-	return fileId, nil
 }
