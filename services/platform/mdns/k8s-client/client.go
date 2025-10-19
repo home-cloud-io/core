1d2660b937cf10b3e7adbf90cd2956313a7d7dc9
Jack Kawell <jack@kawell.us>
2025-10-18 20:10:33 -0500

    better kube client setup

--- services/platform/mdns/k8s-client/client.go
+++ services/platform/mdns/k8s-client/client.go
@@ -1,18 +1,20 @@
 package k8sclient
 
 import (
+	"os"
+
 	"github.com/steady-bytes/draft/pkg/chassis"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func NewClient(logger chassis.Logger) *kubernetes.Clientset {
-	// NOTE: this will attempt first to build the config from the path given in the draft config and will
+	// NOTE: this will attempt first to build the config from the KUBECONFIG env var and then
 	// fallback on the in-cluster config if no path is given
-	config := chassis.GetConfig()
-	kubeConfig, err := clientcmd.BuildConfigFromFlags(config.GetString("server.k8s.master_url"), config.GetString("server.k8s.config_path"))
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
 	if err != nil {
-		logger.WithError(err).Panic("failed to build kube config")
+		logger.WithError(err).Error("failed to read kube config")
+		panic(err)
 	}
 
 	k8sClient, err := kubernetes.NewForConfig(kubeConfig)
