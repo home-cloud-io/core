35e1575b22f540c5799ca213985876242f8d4ea9
Jack Kawell <jack@kawell.us>
2026-01-18 23:47:00 -0600

    remove old daemon code

--- services/platform/daemon/main.go
+++ services/platform/daemon/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/home-cloud-io/core/services/platform/daemon/communicate"
-	"github.com/home-cloud-io/core/services/platform/daemon/host"
-	kvclient "github.com/home-cloud-io/core/services/platform/daemon/kv-client"
 
 	"github.com/steady-bytes/draft/pkg/chassis"
 	"github.com/steady-bytes/draft/pkg/loggers/zerolog"
@@ -11,24 +9,12 @@ import (
 
 func main() {
 	var (
-		logger          = zerolog.New()
-		secureTunneling = host.NewSecureTunnelingController(logger)
-		server          = communicate.New(logger, secureTunneling)
-		migrator        = host.NewMigrator(logger)
+		logger = zerolog.New()
+		server = communicate.New(logger)
 	)
 
-	runner := func() {
-		kvclient.Init()
-	}
-
 	// setup runtime
-	runtime := chassis.New(logger).
+	chassis.New(logger).
 		WithRPCHandler(server).
-		// TODO: this doesn't gaurantee the kvclient will be ready for the other runners
-		WithRunner(runner).
-		WithRunner(migrator.Migrate).
-		WithRunner(secureTunneling.Load)
-
-	// start daemon runtime
-	runtime.Start()
+		Start()
 }
