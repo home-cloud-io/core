1c03b01b69579b597d186f7a96f4a1be45cb8a3f
Jack Kawell <jack@kawell.us>
2026-09-02 08:42:45 -0500

    fixed a copy-pasta with database/user existence checks (also improved database idempotency)

--- cmd/operator/controller/apps/databases.go
+++ cmd/operator/controller/apps/databases.go
@@ -9,6 +9,7 @@ import (
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -121,57 +122,70 @@ func sysObjectExists(ctx context.Context, db *bun.DB, query string) (bool, error
 }
 
 func (r *AppReconciler) createPostgresUser(ctx context.Context, db *bun.DB, d AppDatabase, namespace string) error {
-	pass, err := secrets.Generate(24, true)
-	if err != nil {
+	var (
+		objName = fmt.Sprintf("%s-%s", d.Type, d.Name)
+		pass    []byte
+	)
+
+	// check for existing secret
+	secret := &corev1.Secret{}
+	err := r.Client.Get(ctx, types.NamespacedName{
+		Name:      objName,
+		Namespace: namespace,
+	}, secret)
+	if client.IgnoreNotFound(err) != nil {
 		return err
+	} else {
+		var ok bool
+		pass, ok = secret.Data["password"]
+		if !ok {
+			return fmt.Errorf("database secret contained no 'password' key")
+		}
 	}
 
-	// create kube secret with access credentials
-	var newSecret bool
-	err = r.Client.Create(ctx, &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-%s", d.Type, d.Name),
-			Namespace: namespace,
-		},
-		Type: corev1.SecretTypeOpaque,
-		Data: map[string][]byte{
-			"hostname": []byte("postgres.postgres"),
-			"database": []byte(d.Name),
-			"username": []byte(d.Name),
-			"password": []byte(pass),
-			"port":     []byte("5432"),
-			"uri":      []byte(fmt.Sprintf("postgresql://%s:%s@postgres.postgres:5432/%s?sslmode=disable", d.Name, pass, d.Name)),
-		},
-	})
-	if err == nil {
-		newSecret = true
-	}
-	if client.IgnoreAlreadyExists(err) != nil {
-		return err
-	}
+	// create secret if not found
+	if errors.IsNotFound(err) {
+		pass, err = secrets.Generate(24, true)
+		if err != nil {
+			return err
+		}
 
-	// check if user database already exists (this happens on a reinstall without wiping old data)
-	exists, err := sysObjectExists(ctx, db, fmt.Sprintf("SELECT 1 FROM pg_database WHERE datname = '%s'", d.Name))
-	if err != nil {
-		return err
-	}
-	if !exists {
-		// create user within database
-		_, err = db.ExecContext(ctx, fmt.Sprintf("CREATE USER %s WITH PASSWORD '%s'", d.Name, pass))
+		// create kube secret with access credentials
+		err = r.Client.Create(ctx, &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      objName,
+				Namespace: namespace,
+			},
+			Type: corev1.SecretTypeOpaque,
+			Data: map[string][]byte{
+				// TODO: consider making hostname:port configurable in setting so users can bring their own database
+				"hostname": []byte("postgres.postgres"),
+				"database": []byte(d.Name),
+				"username": []byte(d.Name),
+				"password": []byte(pass),
+				"port":     []byte("5432"),
+				"uri":      []byte(fmt.Sprintf("postgresql://%s:%s@postgres.postgres:5432/%s?sslmode=disable", d.Name, pass, d.Name)),
+			},
+		})
 		if err != nil {
 			return err
 		}
 	}
-	// new secret (maybe the old one was deleted)
-	if exists && newSecret {
-		// update user password within database
+
+	// check if user already exists
+	exists, err := sysObjectExists(ctx, db, fmt.Sprintf("SELECT 1 FROM pg_roles WHERE rolname='%s'", d.Name))
+	if err != nil {
+		return err
+	}
+
+	// update if exists, else create
+	if exists {
 		_, err = db.ExecContext(ctx, fmt.Sprintf("ALTER USER %s WITH PASSWORD '%s'", d.Name, pass))
-		if err != nil {
-			return err
-		}
+	} else {
+		_, err = db.ExecContext(ctx, fmt.Sprintf("CREATE USER %s WITH PASSWORD '%s'", d.Name, pass))
 	}
 
-	return nil
+	return err
 }
 
 func (r *AppReconciler) deletePostgresUser(ctx context.Context, db *bun.DB, d AppDatabase, namespace string) error {
@@ -195,8 +209,8 @@ func (r *AppReconciler) deletePostgresUser(ctx context.Context, db *bun.DB, d Ap
 }
 
 func (r *AppReconciler) createPostgresUserDatabase(ctx context.Context, db *bun.DB, d AppDatabase, secret *corev1.Secret) error {
-	// check if user already exists (this happens on a reinstall without wiping old data)
-	exists, err := sysObjectExists(ctx, db, fmt.Sprintf("SELECT 1 FROM pg_roles WHERE rolname='%s'", d.Name))
+	// check if user database already exists (this happens on a reinstall without wiping old data)
+	exists, err := sysObjectExists(ctx, db, fmt.Sprintf("SELECT 1 FROM pg_database WHERE datname = '%s'", d.Name))
 	if err != nil {
 		return err
 	}
